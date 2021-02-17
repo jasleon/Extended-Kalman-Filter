f17e0410ea984b3ed81e71ffed7b1c76943511b1
Antonio Sanchez <antonio.buap@gmail.com>
2021-02-17 08:36:45 -0600

    feat: Implement EKF update

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -49,4 +49,35 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
    * TODO: update the state by using Extended Kalman Filter equations
    */
+  
+  // Recover state parameters
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+
+  // Map from cartesian to polar coordinates
+  double rho = hypot(px, py);
+  double phi = atan2(px, py);
+  double rho_dot = (px * vx + py * vy) / std::max(rho, 0.0001);
+
+  // Predicted state in polar coordinates
+  VectorXd z_check(3);
+  z_check << rho, phi, rho_dot;
+
+  // Calculate the measurement residual
+  VectorXd y = z - z_check;
+
+  // Normalize angle
+  while(y(1) >  M_PI) y(1) -= 2 * M_PI;
+  while(y(1) < -M_PI) y(1) += 2 * M_PI;
+
+  MatrixXd S = H_ * P_ * H_.transpose() + R_;
+  MatrixXd K = P_ * H_.transpose() * S.inverse();
+
+  // Update state and covariance
+  x_ = x_ + K * y;
+  size_t x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
