10041222fc648ed2815fa673aab981de94d06cc7
Antonio Sanchez <antonio.buap@gmail.com>
2021-02-17 06:48:16 -0600

    feat: Implement Kalman filter prediction

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -26,6 +26,8 @@ void KalmanFilter::Predict() {
   /**
    * TODO: predict the state
    */
+  x_ = F_ * x_;
+  P_ = F_ * P_ * F_.transpose() + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
