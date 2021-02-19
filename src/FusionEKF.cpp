0c2cbc8f2d3617657add1f841ab2d6ce9be3ac91
Antonio Sanchez <antonio.buap@gmail.com>
2021-02-19 07:18:52 -0600

    feat: Update the state transition matrix F

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -100,7 +100,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       float phi = measurement_pack.raw_measurements_[1];
       float rho_dot = measurement_pack.raw_measurements_[2];
 
-      // Map from polar to cartesian coordinates
+      // map from polar to cartesian coordinates
       float px = rho * cos(phi);
       float py = rho * sin(phi);
       float vx = rho_dot * cos(phi);
@@ -133,6 +133,26 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
 
+  // compute the time elapsed between the current and previous measurements
+  // dt - expressed in seconds
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;
+  previous_timestamp_ = measurement_pack.timestamp_;
+
+  float dt_2 = dt * dt;
+  float dt_3 = dt_2 * dt;
+  float dt_4 = dt_3 * dt;
+
+  // modify the F matrix so that the time is integrated
+  ekf_.F_(0, 2) = dt;
+  ekf_.F_(1, 3) = dt;
+
+  // set the process covariance matrix Q
+  ekf_.Q_ = MatrixXd(4, 4);
+  ekf_.Q_ <<  dt_4/4*noise_ax_, 0, dt_3/2*noise_ax_, 0,
+              0, dt_4/4*noise_ay_, 0, dt_3/2*noise_ay_,
+              dt_3/2*noise_ax_, 0, dt_2*noise_ax_, 0,
+              0, dt_3/2*noise_ay_, 0, dt_2*noise_ay_;
+  
   ekf_.Predict();
 
   /**
