3f608ba1c720eee26e7301ea2bb1502ddf12bfca
Antonio Sanchez <antonio.buap@gmail.com>
2021-02-19 07:42:53 -0600

    feat: Use sensor type to perform the update step

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -167,10 +167,14 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // TODO: Radar updates
-
+    ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.R_ = R_radar_;
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
     // TODO: Laser updates
-
+    ekf_.H_ = H_laser_;
+    ekf_.R_ = R_laser_;
+    ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
